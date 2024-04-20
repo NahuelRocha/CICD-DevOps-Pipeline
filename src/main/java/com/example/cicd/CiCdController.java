ff6149202fb30d7b301b3ba946a55c6ed8db3e15
Rocha Nahuel <rocha.nahuel@hotmail.com>
2024-04-20 15:50:10 -0300

    added cv

--- src/main/java/com/example/cicd/CiCdController.java
+++ src/main/java/com/example/cicd/CiCdController.java
@@ -1,14 +1,13 @@
-package com.example.cicd;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CiCdController {
-
-    @GetMapping("/hola")
-    public ResponseEntity<String> getHello(){
-        return ResponseEntity.ok("Hello my friend! Arvind!!!!!!!");
-    }
-}
+//package com.example.cicd;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class CiCdController {
+//
+//    @GetMapping("/")
+//    public String getCV(){
+//        return "index";
+//    }
+//}
